440c948365782d4ac62db4ce825b1d679ef756ed
Kamas 'Iceberg' Lau <kamaslau@outlook.com>
2018-03-24 03:35:14 +0800

    beta20180324

--- application/views/vote/detail-result.php
+++ application/views/vote/detail-result.php
@@ -1,16 +1,45 @@
 <style>
-    .vote-option {width:100%;float:none;}
+    h1 {color:#e9c387;font-size:58px;text-align:center;}
+        h1>span {display:block;font-size:26px;}
+
+    #counter-container {width:562px;margin:36px auto 0;}
+
+    #result-panel {margin-top:36px;border:1px solid #b12839;border-radius:15px;box-shadow:0 0 8px 0 #81131c;padding:0;overflow:hidden;}
+        #ace-options {background-color:#a42535;}
+        #other-options {background-color:#941f2e;}
+
+    .vote-option {color:#fff;background-color:transparent;font-size:28px;width:100%;margin-bottom:48px;border:0;border-radius:0;padding:0;box-shadow:none;float:none;}
         .vote-option:nth-of-type(2n+0) {margin-left:0;}
+        #ace-options .vote-option:first-child {background:url('/media/images/vote/ranking_1.png') no-repeat right 36px;background-size:40px 50px;}
+        #ace-options .vote-option:nth-child(2) {background:url('/media/images/vote/ranking_2.png') no-repeat right 36px;background-size:40px 50px;}
+        #ace-options .vote-option:nth-child(3) {background:url('/media/images/vote/ranking_3.png') no-repeat right 36px;background-size:40px 50px;}
 
         .option-figure {width:105px;height:105px;margin-top:0;border-radius:12px;overflow:hidden;float:left;}
-            .option-figure figure {width:105px;height:105px;}
-        .option-brief {margin-left:105px;}
-            .option-rank {color:#fde101;font-size:24px;float:right;}
+            .option-figure figure {width:100%;height:100%;}
+        .option-brief {font-size:24px;margin-left:102px;padding-top:8px;position:relative;}
+            #other-options .option-brief {margin-left:0;}
+            .option-brief>.ballot-count {margin-bottom:0;}
+                .option-brief>.option-name {height:24px;margin-bottom:20px;}
+            .option-rank {color:#fde101;position:absolute;right:0;top:8px;}
 
 	/* 宽度在750像素以上的设备 */
 	@media only screen and (min-width:751px)
 	{
-		
+		body {width:100%;padding:52px 2.4% 58px;}
+
+        #result-panel {}
+            #result-panel>* {display:inline-block;}
+            #ace-options {width:72%;float:left;overflow:hidden;padding:68px 96px 20px;border-right:1px solid indianred;
+                -webkit-column-count:2;
+                -moz-column-count:2;
+                column-count:2;
+                -webkit-column-gap:75px;
+                -moz-column-gap:75px;
+                column-gap:75px;}
+            #other-options {width:28%;float:right;height:748px;overflow-y:scroll;padding:68px 72px;}
+                #other-options .vote-option {margin-bottom:0;}
+
+        .option-figure {width:84px;height:84px;}
 	}
 	
 	/* 宽度在960像素以上的设备 */
@@ -33,25 +62,13 @@
 	<p><?php echo $error ?></p>
 
 	<?php else: ?>
-    <!-- 背景音乐 -->
-    <?php if ( ! empty($item['url_audio'])): ?>
-    <audio id=vote-audio class=hide autoplay loop alt="背景音乐" src="<?php echo $item['url_audio'] ?>">您的浏览器不支持音频播放</audio>
-    <div id=audio-control><i class="far fa-pause"></i></div>
-    <?php endif ?>
 
     <!-- 投票活动信息 -->
     <div id=vote-info>
-        <?php if ( empty($item['url_image']) ): ?>
-        <h1 id=vote-name><?php echo $item['name'] ?></h1>
-        <?php else: ?>
-        <figure id=vote-url_image class=vote-figure>
-            <img alt="<?php echo $item['name'] ?>形象图" src="<?php echo $item['url_image'] ?>">
-        </figure>
-        <?php endif ?>
+        <h1>投票结果<span>VOTING RESULTS</span></h1>
 
         <div id=vote-brief>
-
-            <div id=counter-container class=vote-time_end>
+            <div id=counter-container class="vote-time_end center_x">
                 <?php
                     // 根据当前时间判断以活动开始时间还是活动结束时间作为倒计时目标时间
                     $time_countdown_ends = (time() >= $item['time_start'])? $item['time_end']: $item['time_start'];
@@ -94,16 +111,18 @@
         </div>
     </div>
 
-    <?php if ( ! empty($options)): ?>
+    <?php
+        if ( ! empty($options)):
+            // 候选项默认占位图
+            $default_option_image = $item['url_default_option_image'];
+    ?>
     <div id=result-panel class=container>
-        <h1>投票结果</h1>
-        <ul id=vote-options>
+        <ul id=ace-options>
             <?php
-                // 候选项默认占位图
-                $default_option_image = $item['url_default_option_image'];
+                // 突出显示前10名之内
+                $ace_counts = (count($options) >= 10)? 10: count($options);
 
-                // 先显示前10名
-                for ($i=0; $i<10; $i++):
+                for ($i=0; $i<$ace_counts; $i++):
                     $option = $options[$i];
             ?>
             <li class=vote-option>
@@ -118,15 +137,35 @@
                     </figure>
                 </a>
                 <div class=option-brief>
-                    <div class=option-rank>第 <?php echo $i + 1 ?> 名</div>
-                    <div class=option-id># <?php echo $option['option_id'] ?></div>
                     <h2 class=option-name><?php echo $option['name'] ?></h2>
                     <div class=ballot-count><?php echo $option['ballot_overall'] ?> 票</div>
+                    <div class=option-rank>第 <?php echo $i + 1 ?> 名</div>
                 </div>
 
             </li>
             <?php endfor ?>
         </ul>
+
+        <?php if (count($options) > $ace_counts): ?>
+        <div id=other-options class="swiper-container">
+            <ul class="swiper-wrapper">
+                <?php
+                    for ($i=$ace_counts; $i<count($options); $i++):
+                        $option = $options[$i];
+                ?>
+                <li class="vote-option swiper-slide">
+
+                    <div class=option-brief>
+                        <h2 class=option-name><?php echo $option['name'] ?></h2>
+                        <div class=ballot-count><?php echo $option['ballot_overall'] ?> 票</div>
+                        <div class=option-rank>第 <?php echo $i + 1 ?> 名</div>
+                    </div>
+
+                </li>
+                <?php endfor ?>
+            </ul>
+        </div>
+        <?php endif ?>
     </div>
     <?php endif ?>
 
@@ -137,3 +176,18 @@
 
 <link rel=stylesheet media=all href="<?php echo CDN_URL ?>jquery/swiper.min.css">
 <script defer src="<?php echo CDN_URL ?>jquery/swiper.min.js"></script>
+<script>
+    window.onload = function() {
+        var swiper = new Swiper('.swiper-container', {
+            //loop: true,
+            direction: 'vertical',
+            slidesPerView: 6,
+            slidesPerGroup: 3,
+            autoHeight: true, //enable auto height
+            autoplay: {
+                delay: 4500,
+                disableOnInteraction: true,
+            }
+        });
+    }
+</script>
